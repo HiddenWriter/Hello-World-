8be57c785c905869d9c7760d7acca04386940fea
HiddenWriter <50237867+HiddenWriter@users.noreply.github.com>
2019-05-10 12:16:26 +0900

    Add files via upload

---
+++ TestClass.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TEST2 {
+
+    public static class Allint {
+        public static void testFunc(int x, int y) {
+            System.Console.Write("X + Y = ");
+            System.Console.Write(x + y);
+        }
+    }
+    public static class AllNames {
+        public static string GetUserName() {
+            return "Howk";
+        }
+    }    
+}
