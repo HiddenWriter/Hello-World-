19e571d67c913638ffa27b9e379cf88db843e65b
HiddenWriter <50237867+HiddenWriter@users.noreply.github.com>
2019-05-12 16:10:59 +0900

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,9 +1,8 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
 namespace TEST2
 {
     class Program
@@ -11,14 +10,11 @@ namespace TEST2
         static void Main(string[] args)
         {   
           var Names = new List<string>{
-            "A" ,"B" , "C"
+            "1" ,"2" , "3"
           };
 
           foreach (var Name in Names) {
-            System.Console.Write($"Massage : {Name} \n");
-            System.Console.Write(TEST2.AllNames.GetUserName());
-            Console.Write("\n");
-            Console.Write("{0:C} + {1:P} + {2}{3}{4}", 10, 2, "and", Name, "\n" );
+            System.Console.Write($"Hello, World!: {Name} \n");
             /* 표준 출력 함수 ex) System.Console.WriteLine이나 System.Console.Write() 같은거 static 으로 선언되어 있다. */
           }
         }
